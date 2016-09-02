3e8b7dcc16fbd04450acdd8db515bd519fb89759
Kevin Dial <kevin.dial.phone@gmail.com>
2016-09-02 01:00:08 -0400

    pretty print statements

--- emir_matrix_problem/solution.py
+++ emir_matrix_problem/solution.py
@@ -6,33 +6,44 @@
 def getLargestSumMatrix(M):
 	N = len(M)
 	largestSum = 0
-	largestSubmatrixTup = None
+	largestSubmatrix = []
 
 	# find all sub matrix permutations
 	for x in range(0, N):
 		for y in range(0, N):
 			for w in range(1, N+1-x):
 				for h in range(1, N+1-y):
-					submatrixTup = (x, y, w, h)
-					temp_sum = sumMatrix(submatrixTup, M)
+					subMatrix = getSubMatrix(x, y, w, h, M)
+					temp_sum = sumMatrix(subMatrix)
 
 					if temp_sum > largestSum:
 						largestSum = temp_sum
-						largestSubmatrixTup = submatrixTup
-	return(largestSum)
+						largestSubmatrix = subMatrix
+	return(largestSum, largestSubmatrix)
 
 
-# Return sum of sub matrix
+# Return sub matrix
 #   x, y are top left coordinates of the sub matrix with respect to the full matrix
 #   w is the width of the sub matrix
 #   h is the height of the sub matrix
-def sumMatrix(submatrixTup, M):
-	(x, y, w, h) = (submatrixTup[0], submatrixTup[1], submatrixTup[2], submatrixTup[3])
-	sum = 0
+def getSubMatrix(x, y, w, h, M):
+	subMatrix = []
 	for i in range(x, x+w):
+		row = []
 		for j in range(y, y+h):
-			sum = sum + M[i][j]
-	return sum
+			row.append(M[i][j])
+		subMatrix.append(row)
+	return subMatrix
+
+def sumMatrix(M):
+	matrixSum = 0
+	for row in M:
+		for item in row:
+			matrixSum += item
+	return matrixSum
+
+
+
 
 
 def printMatrix(M):
@@ -40,8 +51,18 @@ def printMatrix(M):
 		for item in row:
 			print('{0:4d}'.format(item), end="")
 		print()
+	print()
 
-def printSubMatrix(submatrixTup):
+def testMatrix(M):
+	print("---------- Test Case ----------")
+	(largestSum, largestSubmatrix) = getLargestSumMatrix(M)
+	print("The full matrix is the following...")
+	printMatrix(M)
+	print("The submatrix with the largest sum is the following...")
+	printMatrix(largestSubmatrix)
+	print("The sum of the submatrix is", largestSum)
+	print()
+	
 
 
 
@@ -49,12 +70,25 @@ def printSubMatrix(submatrixTup):
 
 
 
-M = [[1, 2, 3], 
+M1 = [[1, 2, 3], 
 	[4, 5, 6], 
 	[7, 8, 9]]
 
+M2 = [[1, 2, 3], 
+	[4, 5, 6], 
+	[7, 8, -9]]
+
+M3 = [[1, 2, 3], 
+	[4, 5, -6], 
+	[7, 8, -9]]
+
+M4 = [[-11, 0, 3], 
+	[4, 5, 6], 
+	[7, -8, 9]]
+
 if __name__ == '__main__':
-	summ = getLargestSumMatrix(M)
-	print(summ)
-	printMatrix(M)
+	testMatrix(M1)
+	testMatrix(M2)
+	testMatrix(M3)
+	testMatrix(M4)
 
